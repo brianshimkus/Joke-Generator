a3d9402e3a70f68885d44e531a8356ba344a975c
Brian Shimkus <brianshimkus1@gmail.com>
2022-07-07 00:48:40 -0500

    Created the useRef hook

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
+import { useRef, useState } from 'react'
 import useRandomJoke from './useRandomJoke'
 
 function App() {
-	const joke = useRandomJoke()
+	const firstNameRef = useRef(null)
+	const lastNameRef = useRef(null)
 
-	// Custom hook
-	// const joke = useRandomJoke('Brian', 'Shimkus')
+	const [firstName, setFirstName] = useState('')
+	const [lastName, setLastName] = useState('')
+
+	const joke = useRandomJoke(firstName, lastName)
+
+	const generateJoke = (e) => {
+		e.preventDefault()
+		setFirstName(firstNameRef.current.value)
+		setLastName(lastNameRef.current.value)
+	}
 
 	return (
 		<div className='bg-slate-700 min-h-screen text-gray-300 text-center p-8 lg:p-24'>
-			<div className='bg-slate-600 border-2 border-slate-500 rounded-md p-12 max-w-xl m-auto'>
+			<div className='bg-slate-600 border-2 border-slate-500 rounded-md p-12 max-w-2xl m-auto'>
 				<h1 className='text-2xl text-rose-300 font-semibold'>Joke Generator</h1>
 				<h2 className='my-12 text-lg'>{joke}</h2>
-				<button className='bg-rose-400 text-rose-900 uppercase font-semibold text-lg p-4 rounded-md hover:bg-rose-300 transition ease-in-out duration-300'>
-					Generate Joke
-				</button>
+				<form className='md:flex'>
+					<input
+						placeholder='First name'
+						ref={firstNameRef}
+						className='bg-slate-700 border-2 border-slate-500 rounded-md p-4 h-16 mr-4 text-rose-300 w-full md:w-min-4 mb-4 md:mb-0'
+					/>
+					<input
+						placeholder='Last name'
+						ref={lastNameRef}
+						className='bg-slate-700 border-2 border-slate-500 rounded-md p-4 h-16 mr-4 text-rose-300 w-full md:w-min-4 mb-4 md:mb-0'
+					/>
+					<button
+						onClick={generateJoke}
+						className='bg-rose-400 text-rose-900 uppercase font-semibold text-lg p-4 rounded-md hover:bg-rose-300 transition ease-in-out duration-300 w-full'>
+						Generate
+					</button>
+				</form>
 			</div>
 		</div>
 	)
--- src/useRandomJoke.js
+++ src/useRandomJoke.js
@@ -1,18 +1,20 @@
 import { useEffect, useState } from 'react'
 
-export default function useRandomJoke() {
+export default function useRandomJoke(firstName, lastName) {
 	const [joke, setJoke] = useState('')
 
 	useEffect(() => {
 		const fetchJoke = async () =>
-			await fetch(`http://api.icndb.com/jokes/random`)
+			await fetch(
+				`http://api.icndb.com/jokes/random?firstName=${firstName}&lastName=${lastName}`
+			)
 				.then((res) => res.json())
 				.then((data) => {
 					setJoke(data.value.joke)
 				})
 
 		fetchJoke()
-	}, [])
+	}, [firstName, lastName])
 
 	return joke
 }
