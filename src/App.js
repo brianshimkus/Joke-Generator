4f727a77ac096e4dfa99afd1f5f2e87877cab481
Brian Shimkus <brianshimkus1@gmail.com>
2022-07-07 00:09:32 -0500

    Built JSON response

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
+import { useEffect } from 'react'
+
 function App() {
+	useEffect(() => {
+		const fetchJoke = async () =>
+			await fetch(`http://api.icndb.com/jokes/random`)
+				.then((res) => res.json())
+				.then((data) => {
+					console.log(data.value.joke)
+				})
+
+		fetchJoke()
+	}, [])
+
 	return (
 		<div className='bg-slate-700 min-h-screen text-gray-300 py-24 text-center'>
 			<h1>Joke Generator</h1>
